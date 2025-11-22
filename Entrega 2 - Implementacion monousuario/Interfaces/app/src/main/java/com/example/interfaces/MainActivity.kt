3c6b415ee5550a0ed2bec539257de1ebb2294cb8
Juan Pabon Vargas <109265994+JohnWilliams2050@users.noreply.github.com>
2025-11-22 12:02:03 -0500

    Enhance MainActivity with comments for clarity
    Added comments for database configuration and light sensor setup.

--- Entrega 2 - Implementacion monousuario/Interfaces/app/src/main/java/com/example/interfaces/MainActivity.kt
+++ Entrega 2 - Implementacion monousuario/Interfaces/app/src/main/java/com/example/interfaces/MainActivity.kt
@@ -11,12 +11,12 @@ import com.example.interfaces.Navigation.Navigation
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.database
-
+//configruarion para acceder a la base de datos
 val PATH_USERS = "users/"
 val firebaseAuth = FirebaseAuth.getInstance()
 val database = Firebase.database
 class MainActivity : ComponentActivity() {
-
+    //configuracion de sensor de luz y tambian empiza la aplicacion y los llega a la seccion de navegacion que los llevaria al login
     @SuppressLint("ServiceCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         val sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
@@ -27,4 +27,5 @@ class MainActivity : ComponentActivity() {
             Navigation()
         }
     }
+
 }
